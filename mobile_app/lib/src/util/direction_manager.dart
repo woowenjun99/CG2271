ff414d0c4252f5f50b744d3cf475ee350521e169
Woo Wen Jun <88195289+woowenjun99@users.noreply.github.com>
2023-10-25 00:31:11 +0800

    Uart logic (#5)
    * Modify the logic
    
    * Squash bugs about the direction
    
    * Fix code for mobile phone
    
    * Fix up the search hotspot logic

--- mobile_app/lib/src/util/direction_manager.dart
+++ mobile_app/lib/src/util/direction_manager.dart
@@ -1,23 +1,68 @@
 import 'dart:math';
 
+class Payload {
+  final bool isForward;
+  final int left;
+  final int right;
+
+  const Payload({
+    required this.isForward,
+    required this.left,
+    required this.right,
+  });
+
+  int _mask(int x) => 1 << x;
+
+  // Computes the payload based on the PWM and the speed to send to the
+  // web socket.
+  int toInteger() {
+    int leftWheel = left;
+    int rightWheel = right;
+
+    // Set the leftWheel to between 0 and 7.
+    if (leftWheel <= -8) leftWheel = -7;
+    if (leftWheel < 0) leftWheel = -leftWheel;
+    if (rightWheel >= 8) rightWheel = 7;
+    if (rightWheel < 0) rightWheel = -rightWheel;
+
+    int payload = leftWheel << 3 | rightWheel;
+    if (isForward) payload |= _mask(6);
+    return payload;
+  }
+}
+
 class DirectionManager {
-  /// Used to compute the angle to turn the robot based on the x and y of the
-  /// joystick value.
-  static int computeAngle(double x, double y) {
-    /// Do not allow for reverse mode.
-    if (y > 0) return 0;
+  final double x;
+  final double y;
+  bool isForward;
 
-    final double angleInRadians = atan2(y, x);
+  DirectionManager({
+    required this.x,
+    required this.y,
+    this.isForward = false,
+  });
 
-    final double angleInDegrees = angleInRadians * (180 / pi);
-
-    int offsetAngle = (angleInDegrees + 90).round();
-
-    if (offsetAngle < -90) {
-      return -90;
-    } else if (offsetAngle > 90) {
-      return 90;
+  Payload computePayload() {
+    // Stop Direction
+    if (x == 0 && y == 0) {
+      return const Payload(isForward: false, left: 0, right: 0);
     }
-    return offsetAngle;
+
+    final double angle = atan2(y, x) * (180 / pi);
+
+    // Forward Direction
+    if (angle >= 0) {
+      return Payload(
+        isForward: true,
+        left: (angle / 180 * 8).floor(),
+        right: ((180 - angle) / 180 * 8).round(),
+      );
+    }
+
+    return Payload(
+      isForward: false,
+      left: ((-angle / -180) * 8).floor(),
+      right: ((-180 - angle) / -180 * 8).floor(),
+    );
   }
 }
