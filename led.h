2f0affd4ded3e15e173168bf11edb6288a446e47
Er Jun Ze <junzeer@u.nus.edu>
2023-10-25 17:31:08 +0800

    Finish LED implementation

--- led.h
+++ led.h
@@ -9,8 +9,30 @@ void initGPIO() {
         RUNNING_LED_PORTS[i]->PCR[RUNNING_LED_PINS[i]] |= PORT_PCR_MUX(1);
         RUNNING_LED_GPIOS[i]->PDDR |= MASK(RUNNING_LED_PINS[i]);
     }
+    
+    FIXED_LED_PORT->PCR[FIXED_LED_PIN] &= ~PORT_PCR_MUX_MASK;
+    FIXED_LED_PORT->PCR[FIXED_LED_PIN] |= PORT_PCR_MUX(1);
+    FIXED_LED_GPIO->PDDR |= MASK(FIXED_LED_PIN);
 }
 
  void ledThread(void* argument) {
+     initGPIO();
+     uint8_t ledPtr = 0;
      
+     while (1) {
+         FIXED_LED_GPIO->PTOR |= MASK(FIXED_LED_PIN);
+         if (leftWheelDutyCycle || rightWheelDutyCycle) {
+             for (int i = 0; i < 8; i++) {
+                 RUNNING_LED_GPIOS[i]->PCOR |= MASK(RUNNING_LED_PINS[i]);
+             }
+             RUNNING_LED_GPIOS[ledPtr]->PSOR |= MASK(RUNNING_LED_PINS[ledPtr]);
+             ledPtr = ledPtr == 7 ? 0 : ledPtr + 1;
+             osDelay(5000);
+         } else {
+             for (int i = 0; i < 8; i++) {
+                 RUNNING_LED_GPIOS[i]->PSOR |= MASK(RUNNING_LED_PINS[i]);
+             }
+             osDelay(2500);
+         }
+     }
  }
